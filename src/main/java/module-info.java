ea18e6c776b457120eb503674fab8094e9c64dfe
Massoud5 <massoud.shams.2016@gmail.com>
2023-11-08 14:34:18 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.tjav501stg_12 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.tjav501stg_12 to javafx.fxml;
+    exports com.example.tjav501stg_12;
+}
