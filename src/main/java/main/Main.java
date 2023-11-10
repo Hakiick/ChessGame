3f26d5515621fe30896acd0d57d1d360249f72ca
Massoud5 <massoud.shams.2016@gmail.com>
2023-11-10 16:04:39 +0100

    first window added

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,26 +1,37 @@
-// Fichier Main.java
 package main;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Échiquier avec Bordure");
+            JFrame frame = new JFrame("CHESS FIGHTER");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            CardLayout cardLayout = new CardLayout();
+            JPanel mainPanel = new JPanel(cardLayout);
+
             Board board = null;
             try {
                 board = new Board();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            } catch (Exception e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(frame, "Failed to load the board: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                System.exit(1);
             }
-            frame.add(board);
-            frame.pack(); // Adjust the size of the frame based on its components
-            frame.setResizable(false); // Prevent window resizing
-            frame.setLocationRelativeTo(null); // Center the window on the screen
-            frame.setVisible(true); // Make the window visible
-            // SVG image loading is removed because Swing doesn't support SVG
+
+            Menu menu = null;
+            menu = new Menu(frame, board, cardLayout, mainPanel);
+            mainPanel.add(menu, "Menu");
+            mainPanel.add(board, "Board");
+
+            frame.add(mainPanel);
+            frame.pack();
+            frame.setResizable(false);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
         });
     }
 }
